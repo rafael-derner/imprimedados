c4af19dc9bbfbbf1cdbc2640376c4f8014b46a90
Rafael Derner <rafael-derner@live.com>
2022-03-16 18:56:22 -0300

    Inset input on google.com

--- imprimedados.js
+++ imprimedados.js
@@ -1,6 +1,12 @@
 
 document.body.style.border = "5px solid red";
 
-let teste = document.querySelectorAll("[type=submit]");
-debugger;
-console.log(teste);
+let centerDiv = document.body.getElementsByTagName("center");
+let printButton = document.createElement("input");
+printButton.setAttribute("value", "Imprimir");
+printButton.setAttribute("type", "submit");
+printButton.setAttribute("class", centerDiv[1].children[1].getAttribute("class"));
+
+console.log(centerDiv[1].children[1].getAttribute("class"));
+
+centerDiv[1].appendChild(printButton);
